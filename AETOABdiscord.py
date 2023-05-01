12df0b61924b78ab6301f07945d7076997f5308d
ANGELELF708 <124015093+ANGELELF708@users.noreply.github.com>
2023-04-30 21:53:11 -0700

    Added Text to Speech in Voice Channels

--- AETOABdiscord.py
+++ AETOABdiscord.py
@@ -6,25 +6,24 @@ import AETOABsource
 from AETOABsource import respond, setup
 import pyttsx3
 import engineio
-from discord import FFmpegPCAudio # Import FFmpegPCAudio from the discord library
+from discord import FFmpegPCMAudio # Import FFmpegPCAudio from the discord library
 import requests # Import the requests library
 import json # Import the json library
 import ffmpeg
 
 #--SPEECH RECOGNITION--
-with open('speechResults.json', 'r') as f:
-  speechResults = json.load(f)
+#with open('speechResults.json', 'r') as f:
+  #speechResults = json.load(f)
 
 #--AETOAB STUFF--
 setup()
 #--DISCORD STUFF--
-discordToken = "MTEwMDIyODU0ODUyODE3NzI4Mg.Gr73ev.ueznh59YeYVW0Zz_w8cdl7tqV206rN1F-ldfBU"
+discordToken = "MTEwMDIyODU0ODUyODE3NzI4Mg.GN-acC.309bR5C7bKIqlziyhPd1ZUx6hvpoTOmh6Cfegg"
 discordChannelID = 1098455638289428550
 
 bot = commands.Bot(command_prefix="ae!", intents=discord.Intents.all())
 
 #--TTS STUFF
-TTString = "Hello world!"
 
 engineio = pyttsx3.init()
 voices = engineio.getProperty('voices')
@@ -33,10 +32,9 @@ engineio.setProperty('rate', 150)
 
 def TTSave(TTString):
     engineio.save_to_file(TTString, 'TTString.mp3')
+    engineio.runAndWait()
 
-TTSave(TTString)
-
-contentFilter = [" nigger", " nigga", " fuck", " shit", " bitch", " motherfucker", " ass", " asshole", " dick", " pussy", " faggot", " fag", " nazi", " nager", " osama", " laden", " hitler", " twin towers", " osama bin laden", " fa.g", " meth", " naeger", " neger"]
+contentFilter = ["nigger", "nigga", "fuck", "shit", "bitch", "motherfucker", "ass", "asshole", "dick", "pussy", "faggot", "fag", "nazi", "nager", "osama", "laden", "hitler", "twin towers", "osama bin laden", "fa.g", "monkey", "meth", "naeger", "neger"]
 
 @bot.command()
 async def s(ctx, *, arg):
@@ -44,17 +42,23 @@ async def s(ctx, *, arg):
     response = respond(arg)
     for word in contentFilter:
         response = response.lower()
-        response = response.replace(word, " *")
+        response = response.replace(word, "*")
     print(response)
     await ctx.send(str(response))
+    if(isConnected):
+        TTSave(response)
+        voice = ctx.voice_client
+        #voice = await channel.connect()
+        source = FFmpegPCMAudio("TTString.mp3")
+        player = voice.play(source)
 
 @bot.command()
 async def join(ctx):
     if(ctx.author.voice):
         channel = ctx.message.author.voice.channel
         voice = await channel.connect()
-        #while vc(): # This creates an infinite loop as long as the bot is connected to a voice channel (maybe change to true idk)
-        voice.play(discord.FFmpegPCMAudio(executable="C:/Path_Program.ffmpeg.exe", source="TTString.mp3"))
+        source = FFmpegPCMAudio("TTString.mp3")
+        player = voice.play(source)
         #pass    
     else:
         ctx.send("Join a VC first dumbass!")
@@ -66,5 +70,9 @@ async def leave(ctx):
         await ctx.send("Bye everyone!")
     else: 
         await ctx.send("I'm not even in VC. Shut up loser!")
+
+def isConnected(ctx):
+    voice_client = ctx.bot.voice_clients
+    return voice_client and voice_client.is_connected()
 
 bot.run(discordToken)
