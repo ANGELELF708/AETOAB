bda7dedc1720336588eea528782b75700bdbc4a1
ANGELELF708 <124015093+ANGELELF708@users.noreply.github.com>
2023-05-06 17:45:46 -0700

    Update AEChatBot.py

--- AEChatBot.py
+++ AEChatBot.py
@@ -1,30 +1,21 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 import speech_recognition
 import pyttsx3
 import engineio
+from chatterbot.trainers import ListTrainer
 import AETOABChatDatasetList
 from AETOABChatDatasetList import datasetList, datasetListUnchecked
+
 import time
-time.clock = time.time 
-
-#USE TKINTER FOR GUI IN FUTURE
-
+time.clock = time.time
+from AETOABsource import respond, setup
 #--CHATBOT STUFF--
-chatbot = ChatBot("AETOAB-Chan") #creates the bot object and names it
-
-#Training: 
-trainer = ListTrainer(chatbot)
-for index in datasetList:
-    trainer.train([
-        index.query, index.response
-    ])
-
+setup
 #set-up
 exit_conditions = (":q", "quit", "exit")
 
-AETOABChan = ["i-80 john", "youtube", "80's song", "i-80", "dub john", "tub john", "a chong", "age of john", "280 john", "8010", "hey john", "uh-oh john"] #use this list for voice recognition correction later
-contentFilter = ["nigger", "nigga", "fuck", "shit", "bitch", "motherfucker", "ass", "asshole", "dick", "pussy"] #ONLY HERE TO STOP IT FROM USING AND LEARNING BAD WORDS!!!!!!!
+AETOABChan = ["i-80 john", "youtube", "80's song", "i-80", "dub john", "tub john", "a chong", "age of john", "280 john"] #use this list for voice recognition correction later
+
 recognizer = speech_recognition.Recognizer()
 
 engineio = pyttsx3.init()
@@ -39,8 +30,8 @@ def speak(text):
 speak("Booting up...")
 
 #Session loop
-while True: #replace while true with function
-#if get speech recognition
+while True:
+    
     try:
         with speech_recognition.Microphone() as mic:
   
@@ -57,21 +48,11 @@ while True: #replace while true with function
         recognizer = speech_recognition.Recognizer()
         continue
     print(textCorrected)
-    query = textCorrected
-    if query in exit_conditions:
-        break
-    else:
-        response = chatbot.get_response(query)
-        for index in contentFilter:
-            responseFiltered = str(response.replace(index, "*"))
-        print(str(responseFiltered))
-        speak(str(responseFiltered))
-        newData = AETOABChatDatasetList.ChatDataset(query, response)
-        datasetListUnchecked.append(newData) #update in future to save this dataset for review and possible inclusion in datasetList
-        #recur function
-    #else check twitch/discord 
-        #recur function
-#--Save File--
+    response = respond(textCorrected)
+    print(response)
+    speak(response)
+
+#--Save File saving--
 with open ('chatbotDatasetListUnchecked.txt', 'w') as f:
     for index in datasetListUnchecked:
         f.write(index.query)
